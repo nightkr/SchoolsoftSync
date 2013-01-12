8fdb393ef546ed0516e6b57b37fa396ef6896fb3
Nullable <teo@nullable.se>
2013-01-12 20:29:51 +0100

    Try to load database URL from environment variable

--- schoolsoftsync/dev_settings.py
+++ schoolsoftsync/dev_settings.py
@@ -1,2 +1,4 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://schoolsoftsync:sss@localhost/schoolsoftsync'
+import os
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://schoolsoftsync:sss@localhost/schoolsoftsync')
 SECRET_KEY='Hey, there!'
