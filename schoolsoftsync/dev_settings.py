e964e585f4ba0d48a0dbe118e160bc816e359c1e
Nullable <teo@nullable.se>
2013-01-12 20:52:36 +0100

    Fix getting the db URL

--- schoolsoftsync/dev_settings.py
+++ schoolsoftsync/dev_settings.py
@@ -1,4 +1,4 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://schoolsoftsync:sss@localhost/schoolsoftsync')
+SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_COPPER_URL', 'postgresqlD://schoolsoftsync:sss@localhost/schoolsoftsync')
 SECRET_KEY='Hey, there!'  # Okay, since we don't store any secret state
