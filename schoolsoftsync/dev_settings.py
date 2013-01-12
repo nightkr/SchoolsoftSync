a26637d078b96fd512e7879db56f7a7abe2a09a9
Nullable <teo@nullable.se>
2013-01-12 20:37:41 +0100

    Add comment explaining the SECRET_KEY

--- schoolsoftsync/dev_settings.py
+++ schoolsoftsync/dev_settings.py
@@ -1,4 +1,4 @@
 import os
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://schoolsoftsync:sss@localhost/schoolsoftsync')
-SECRET_KEY='Hey, there!'
+SECRET_KEY='Hey, there!'  # Okay, since we don't store any secret state
